d060060d1603e076c02143fa8557e08987b91d41
xoandaica <kiendt@vnpt-technology.vn>
2017-01-03 17:34:42 +0700

    add new project

--- application/controllers/MenuController.php
+++ application/controllers/MenuController.php
@@ -3,12 +3,15 @@
 class MenuController extends Zend_Controller_Action {
 
     public function init() {
-        $response = $this->getResponse();
-        $response->insert('menuBar', $this->view->render('menuBar.phtml'));
+//        $response = $this->getResponse();
+        $this->view->current = "Product";
+//        $response->insert('menuBar', $this->view->render('menuBar.phtml'));
     }
 
     public function indexAction() {
+//        $this->_helper->layout->setLayout('layout');
         // action body
+        
     }
 
 }
